7853cdd4f78274735e3cab11edc3024ca5a6a6d4
Viktor Ferenczi <ferencziviktor11@gmail.com>
2021-05-27 22:13:23 +0200

    insert git into welcome message

--- messages/messageList.js
+++ messages/messageList.js
@@ -1,6 +1,6 @@
 let welcomeMessage = "Üdvözöllek! \n A Dogvision Discord szerver botja vagyok. Funkcióim jelenleg fejlesztés alatt állnak, " +
 "ezért még nem tudok sajnos sokat nyújtani neked. Ha van bármilyen ötleted, " +
-"esetleg fejleszteni akarsz engem (rendelkezel Javascript, Node.js tudással) nyugodtan keresd meg a gazdámat, akit Captain Flint néven találsz. " +
+"esetleg fejleszteni akarsz engem (rendelkezel Javascript, Node.js tudással) nyugodtan keresd meg a gazdámat, akit Captain Flint néven találsz. Itt megtalálsz: https://github.com/viktorferenczi/Discord-Bot" +
 "\n \nCommandjaim: " +
 "\n??nb - Következő BDO Boss éledése a szerveren: EU\n " +
 
@@ -9,7 +9,7 @@ let welcomeMessage = "Üdvözöllek! \n A Dogvision Discord szerver botja vagyok
 "\n--------------------------------------------------------------------------------------------------------------------------------------------------------\n " +
 
 "Hey! \n I am the Dogvision Discord server's bot. My functions are under development right now, so I can offer you less help during this time. " +
-"If you have a good idea or you want to develop me (have knowledge in Javascript, Node.js) feel free to contact my master, Captain Flint. " +
+"If you have a good idea or you want to develop me (have knowledge in Javascript, Node.js) feel free to contact my master, Captain Flint. You can find me here: https://github.com/viktorferenczi/Discord-Bot" +
 "\n \nCommands: " +
 "\n??nb - Next BDO boss spawn in server: EU\n " +
 
